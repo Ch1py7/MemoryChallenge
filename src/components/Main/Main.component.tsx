929fda73b6ec4b37cfefed9b5b9f8844f1006ecb
Ch1py7 <garcialopezgerardo6612@gmail.com>
2023-07-04 16:22:50 -0600

    fix: react router dom

--- src/components/Main/Main.component.tsx
+++ src/components/Main/Main.component.tsx
@@ -1,16 +1,17 @@
 import { Logo } from 'assets'
+import { Link } from 'react-router-dom'
 import { FC, ReactElement } from 'react'
 
 export const Main: FC = (): ReactElement => {
   return (
     <div className='w-full h-full flex justify-center items-center flex-col gap-10'>
       <img src={Logo} className='w-[40rem] animate-slideIn h-64' />
-      <a
-        href='/game'
+      <Link
+        to='/game'
         className='text-gray-900 text-7xl bg-stone-50 px-10 py-4 rounded-lg animate-slideUp hover:transformY hover:-translate-y-1'
       >
         Start!
-      </a>
+      </Link>
       <audio autoPlay loop>
         <source src='/background.mp3' type='audio/mp3' />
       </audio>
